ac9e6673e6725b0154eb7a8b21750974f2e10c57
Khánh <khanhquang0912qh@gmail.com>
2026-07-14 11:21:47 +0700

    fix: update banner

--- Runtime/Enum/BannerPosition.cs
+++ Runtime/Enum/BannerPosition.cs
@@ -2,7 +2,14 @@ namespace com.ktgame.ads.core
 {
     public enum BannerPosition
     {
-        Top = 0,
-        Bottom = 1
+        TopLeft = 0,
+        TopCenter = 1,
+        TopRight = 2,
+        Centered = 3,
+        CenterLeft = 4,
+        CenterRight = 5,
+        BottomLeft = 6,
+        BottomCenter = 7,
+        BottomRight = 8
     }
 }
